a6c2c486b745aa9302f0537a06895c961961663b
unknown <zborowsk@student.agh.edu.pl>
2023-10-13 11:07:10 +0200

    Exercises 17-22

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -1,4 +1,21 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
 public class OptionsParser {
+    public static MoveDirection[] Parse(String[] args) {
+        MoveDirection[] result = new MoveDirection[args.length];
+
+        for(int i=0; i<args.length; i++) {
+            switch(args[i]) {
+                case "f": result[i] = MoveDirection.FORWARD;
+                case "b": result[i] = MoveDirection.BACKWARD;
+                case "r": result[i] = MoveDirection.RIGHT;
+                case "l": result[i] = MoveDirection.LEFT;
+                default: result[i] = null;
+            }
+        }
+
+        return result;
+    }
 }
