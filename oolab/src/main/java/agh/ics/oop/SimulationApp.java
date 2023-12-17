b9fe123d6181eda97e385505b39ddfe4b95fbb13
unknown <zborowsk@student.agh.edu.pl>
2023-12-17 18:00:42 +0100

    Put drawMap in Platform.runLater

--- oolab/src/main/java/agh/ics/oop/SimulationApp.java
+++ oolab/src/main/java/agh/ics/oop/SimulationApp.java
@@ -6,6 +6,7 @@ import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2D;
 import agh.ics.oop.presenter.SimulationPresenter;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -14,22 +15,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.List;
 
+
 public class SimulationApp extends Application {
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
         BorderPane viewRoot = loader.load();
         SimulationPresenter presenter = loader.getController();
-        configureState(primaryStage,viewRoot);
+
 
         List<Vector2D> positions = List.of(new Vector2D(2,2), new Vector2D(3,4));
         List<MoveDirection> directions = OptionsParser.parse(getParameters().getRaw());
         GrassField grassMap = new GrassField(10);
-        grassMap.addObserver(new SimulationPresenter());
-        grassMap.addObserver(new ConsoleMapDisplay());
-        new Simulation(directions, positions, grassMap).run();
-        SimulationPresenter presenter1 = new SimulationPresenter();
+
         presenter.setWorldMap(grassMap);
+        configureState(primaryStage,viewRoot);
+
+        grassMap.addObserver(new SimulationPresenter());
+        new Simulation(directions, positions, grassMap).run();
 
         primaryStage.show();
     }
--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -2,11 +2,13 @@ package agh.ics.oop.presenter;
 
 import agh.ics.oop.model.MapChangeListener;
 import agh.ics.oop.model.WorldMap;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class SimulationPresenter implements MapChangeListener {
     WorldMap map;
+
     @FXML
     private Label infoLabel;
 
@@ -14,12 +16,12 @@ public class SimulationPresenter implements MapChangeListener {
         this.map = map;
     }
 
-    private void drawMap() {
+    private void drawMap(WorldMap map) {
         infoLabel.setText(map.toString());
     }
 
+    @Override
     public void mapChanged(WorldMap map, String text) {
-        setWorldMap(map);
-        drawMap();
+        Platform.runLater(() -> drawMap(map));
     }
 }
