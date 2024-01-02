0854adead35722b78f950469124aa41ce78d33db
unknown <zborowsk@student.agh.edu.pl>
2024-01-02 21:03:09 +0100

    Added random initial positions and random genomes in Simulation

--- projektC2/src/main/java/agh/ics/projektC2/Simulation.java
+++ projektC2/src/main/java/agh/ics/projektC2/Simulation.java
@@ -3,6 +3,7 @@ package agh.ics.projektC2;
 import agh.ics.projektC2.model.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -13,14 +14,19 @@ public class Simulation implements Runnable {
     private final int plantCount;
     private static final Random PRNG = new Random();
 
-    public Simulation(List<Vector2D> positions, WorldMap map, int plantCount) {
+    public Simulation(WorldMap map, int plantCount, int animalsCount, int initialEnergy, int genomeLength) {
         this.map = map;
         this.plantCount = plantCount;
+        List<Vector2D> positions = new PositionGenerator(map.getCurrentBounds(),new HashMap<>()).getPositions();
+        positions = new RandomPositionGenerator(positions,new ArrayList<>(),animalsCount).getPositions();
 
-
-        for(Vector2D position: positions) {
+        for(Vector2D position : positions) {
             try {
-                Animal animal = new Animal(position,100,List.of(PRNG.nextInt(8),PRNG.nextInt(8),PRNG.nextInt(8),PRNG.nextInt(8),PRNG.nextInt(8),PRNG.nextInt(8),PRNG.nextInt(8),PRNG.nextInt(8)));
+                List<Integer> genome = new ArrayList<>();
+                for(int i=0; i<genomeLength; i++) {
+                    genome.add(PRNG.nextInt(8));
+                }
+                Animal animal = new Animal(position,initialEnergy,genome);
                 map.place(animal);
             } catch (PositionAlreadyOccupiedException e) {
                 System.out.println("Can't place an animal at position " + position.toString());
--- projektC2/src/main/java/agh/ics/projektC2/World.java
+++ projektC2/src/main/java/agh/ics/projektC2/World.java
@@ -10,7 +10,7 @@ public class World {
         EarthMap map = new EarthMap(10,10,1,20,10,mutation);
         map.addObserver(new ConsoleMapDisplay());
         List<Vector2D> positions = List.of(new Vector2D(0,0),new Vector2D(2,2), new Vector2D(5,5), new Vector2D(4,4), new Vector2D(7,5), new Vector2D(8,9));
-        Simulation simulation = new Simulation(positions,map,3);
+        Simulation simulation = new Simulation(map,2,5,100,10);
 
         simulation.run();
     }
